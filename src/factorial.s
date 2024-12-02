4ec6d1d192d24d65c403099c561dc42354c1060c
Jaspe Michael <micaeljaspe@gmail.com>
2024-12-02 19:08:55 +0200

    feat: added comments to factorial.s

--- src/factorial.s
+++ src/factorial.s
@@ -4,11 +4,11 @@
 .globl factorial
 
 _start:
-	pushl $5
-	call factorial
-	addl $5, %esp
-	movl %eax, %ebx
-	movl $1, %eax
+	pushl $5  # Push the parameter to the stack
+	call factorial  # Call the function `factorial`
+	addl $4, %esp  # Scrub the parameter on the stack
+	movl %eax, %ebx  # Store the result in %ebx
+	movl $1, %eax  # Store the `exit` syscall number in %eax
 	int $0x80
 
 .type factorial, @function
@@ -22,7 +22,7 @@ factorial:
 	pushl %eax
 	call factorial
 	movl 8(%ebp), %ebx
-	imull %ebx, %eax
+	imull %ebx, %eax  # Store the result of mull in %eax
 
 end_factorial:
 	movl %ebp, %esp
